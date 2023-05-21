b5a3c6162345ef6e696556dcc3b75f679f6e5d3e
Nicole Marinho Espindola <nicole.m.espindola@gmail.com>
2023-05-21 17:39:25 -0300

    primeiro commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+    const menuLinks = document.getElementById("links");
+    if (menuLinks.style.display === "block") {
+      menuLinks.style.display = "none";
+    } else {
+      menuLinks.style.display = "block";
+    }
+  }
